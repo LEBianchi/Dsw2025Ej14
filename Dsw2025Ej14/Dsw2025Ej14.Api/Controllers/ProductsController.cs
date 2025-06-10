1bfe36e65211e932a17b3d59841a9ef1ccaa428d
LEBianchi <Lisandro.BianchiChaban@alu.frt.utn.edu.ar>
2025-06-10 19:06:57 -0300

    Trabajo practico 14

---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Dsw2025Ej14.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Ej14.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ProductsController : ControllerBase
+    {
+        private readonly IPersistenciaEnMemoria _persistencia;
+
+        public ProductsController(IPersistenciaEnMemoria persistencia)
+        {
+            _persistencia = persistencia;
+        }
+        [HttpGet()]
+
+        public IActionResult GetProducts()
+        {
+
+            return Ok(_persistencia.GetProducts());
+        }
+        [HttpGet("{sku}")]
+        public IActionResult GetProduct(string sku)
+        {
+            var products = _persistencia.GetProducts();
+            if (products == null) return NotFound();
+            return Ok(_persistencia?.GetProduct(sku));
+        }
+    }
+}
---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Data/PersistenciaEnMemoria.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Dsw2025Ej14.Api.Domain;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class PersistenciaEnMemoria : IPersistenciaEnMemoria
+    {
+        private List<Product> _products = [];
+        private bool _cargando = false;
+
+
+        public PersistenciaEnMemoria()
+        {
+            LoadProducts();
+        }
+
+        public Product? GetProduct(string sku)
+        {
+           return _products.FirstOrDefault(p => p.Sku == sku);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _products.Where(p => p.IsActive);
+        }
+        public void LoadProducts()
+        {
+            var json = File.ReadAllText("products.json");
+            var productos = JsonSerializer.Deserialize<List<Product>>(json, new JsonSerializerOptions
+            { PropertyNameCaseInsensitive = true, }
+            ) ?? [];
+            _products = productos;
+        }
+    }
+}
